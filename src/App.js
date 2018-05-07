f5a5e32bc44899129bb5121d24fe5824c0fd778e
Pretzi <manuel@ciudaliapp.com>
2018-05-06 22:06:18 -0500

    Add component logic

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { FormGroup, Button, Spinner } from "@blueprintjs/core";
+import axios from 'axios';
 import Input from './components/common/input/Input';
 import RandomGifs from './components/random-gifs/RandomGifs';
 import PropTypes from 'prop-types';
@@ -22,6 +23,10 @@ class RandomGifsComponent extends Component {
       numberOfImages,
       gifs
     };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.fetchGif = this.fetchGif.bind(this);
   }
 
   handleInputChange(e) {
@@ -32,24 +37,85 @@ class RandomGifsComponent extends Component {
 
   handleSubmit(e) {
     e.preventDefault();
+    this.setState({
+      loading: true,
+      notFound: false,
+      error: false
+    }, () => {
+      this.fetchGif();
+    });
+  }
 
+
+  fetchGif(gifs = []) {
     const {
       category,
       numberOfImages,
-      timer
+      timer,
     } = this.state;
 
+    const counter = [...gifs];
+    let error = false;
+    let notFound = false;
+
+    axios.get('https://api.giphy.com/v1/gifs/random', {
+      params: {
+        api_key: 'h7XYSMDQGAe7jdli56bt5jvHeE6BVR2m',
+        tag: category
+      }
+    })
+      .then(response => {
+        if (response.data.data) {
+          const gif = response.data.data;
+
+          gif.length !== 0
+            ? counter.push(gif)
+            : notFound = true;
+        }
+        else {
+          error = true
+        }
+
+        if (notFound || error) {
+          this.setState({
+            notFound,
+            error,
+            gifs: [],
+            loading: false
+          }, () => false)
+        }
+        else {
+          counter.length !== Number(numberOfImages)
+            ? this.fetchGif(counter)
+            : this.setState({
+              gifs: counter,
+              loading: false
+            }, () => {
+              setTimeout(() => {
+                this.setState({
+                  loading: true
+                }, this.fetchGif());
+              }, timer * 1000)
+            });
+        }
+      })
   }
 
   render() {
     const {
-      gifs,
       showNumberOfImages,
       showTimer,
-      loading,
-      notFound,
-      error
     } = this.props
+
+    const {
+      notFound,
+      error,
+      loading,
+      gifs,
+      category,
+      numberOfImages,
+      timer
+    } = this.state;
 
     return (
       <div className="RandomGifsComponent">
@@ -62,7 +128,7 @@ class RandomGifsComponent extends Component {
           >
             <FormGroup label="Gif Category">
               <Input
-                value={this.state.category}
+                value={category}
                 onChange={e => this.handleInputChange(e)}
                 placeholder="Tacos"
                 type="string"
@@ -74,7 +140,7 @@ class RandomGifsComponent extends Component {
             {showNumberOfImages &&
               <FormGroup label="Number Of Images">
                 <Input
-                  value={this.state.numberOfImages}
+                  value={numberOfImages}
                   onChange={e => this.handleInputChange(e)}
                   placeholder="3"
                   name="numberOfImages"
@@ -87,7 +153,7 @@ class RandomGifsComponent extends Component {
             {showTimer &&
               <FormGroup label="Timer" helperText="Time is in seconds">
                 <Input
-                  value={this.state.timer}
+                  value={timer}
                   onChange={e => this.handleInputChange(e)}
                   placeholder="10 seconds"
                   name="timer"
